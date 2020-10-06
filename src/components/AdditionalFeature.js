b63871f55cb19c6ac390b19c737d1888136b34a8
Brenson <brensonw300@gmail.com>
2020-10-06 08:07:19 -0700

    Added code inside additional feature and features files

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const AdditionalFeature = props => {
+      //console.log('tl: additional feature (single) props:', props);
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={() => props.buy(props.feature)}  className="button"><i className="fas fa-plus-circle"></i></button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
